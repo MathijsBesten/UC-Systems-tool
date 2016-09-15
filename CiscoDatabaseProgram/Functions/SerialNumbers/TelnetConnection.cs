d63e88a6055e1b9f3aafc222b71b914ce70928cb
Mathijs <m88_db@hotmail.com>
2016-09-15 11:15:31 +0200

    changed process to get serial number

--- CiscoDatabaseProgram/Functions/SerialNumbers/TelnetConnection.cs
+++ CiscoDatabaseProgram/Functions/SerialNumbers/TelnetConnection.cs
@@ -14,7 +14,7 @@ namespace CiscoDatabaseProgram.Functions.SerialNumbers
 {
     class TelnetConnection
     {
-        public static void telnetClientSockets(string IPAddressString)
+        public static void telnetClientSockets(string IPAddressString) // function will not work for cisco router
         {
             List<string> log = new List<string>();
             IPAddress address;
@@ -58,18 +58,21 @@ namespace CiscoDatabaseProgram.Functions.SerialNumbers
                 Logging.Exit.defaultExit();
             }
         }
-        public static void telnetClientTCP(string IPAddressString)
+        public static void telnetClientTCP(string IPAddressString) // telnet client using tcp client
         {
             //initialze all values
             List<string> log = new List<string>(); // list for log
-            IPAddress address; // future ip
-            string message = "mathijs\r\ndenbesten\r\nshow diag\r\n"; // message in string
+            IPAddress address; // will be filled after conversion
+            string command = "sh diag | inc Serial"; // command to get serialnumber
+            string message = PrivateValues.testRouterUsername + "\r\n"+ PrivateValues.testRouterPassword +"\r\n"+ command + "\r\n"; // command to run
             byte[] messageInBytes; // message in bytes
             byte[] responseInBytes = new byte[4096]; // need a big byte array because much data
             string response; // response in string
             NetworkStream stream;
 
-
+            log.Add("Getting serialnumber...");
+            Logging.Logs.writeToLogfile(log);
+            log.Clear();
             bool convertIP = IPAddress.TryParse(IPAddressString, out address); //Try convert ip address
             if (convertIP == true) // if conversion was successfull
             {
@@ -82,12 +85,14 @@ namespace CiscoDatabaseProgram.Functions.SerialNumbers
                     stream = client.GetStream(); // talking line of client
                     stream.Write(messageInBytes, 0, messageInBytes.Count()); // send data to router
                     // data send
-                    Thread.Sleep(10); // temporarily solution to the incomplete data  
-                    int bytes = stream.Read(responseInBytes, 0, responseInBytes.Length);
-                    response = Encoding.ASCII.GetString(responseInBytes, 0, bytes);
-                    Console.WriteLine(response);
-                    string chassisSerialNumber = findCereal(response);
-                    Console.WriteLine(chassisSerialNumber);
+                    Thread.Sleep(50); // temporarily solution to the incomplete data  
+                    int bytes = stream.Read(responseInBytes, 0, responseInBytes.Length); // read data is put in byte[] responseInBytes
+                    response = Encoding.ASCII.GetString(responseInBytes, 0, bytes); // byte array is decoded to string using ascii code
+                    Console.WriteLine(response); // response string will be echo't
+                    string chassisSerialNumber = findCereal(response); // serialnumber will be received by using substring method
+                    Console.WriteLine(chassisSerialNumber); // serialnumber is echo't
+                    log.Add("Serialnumber received - IP Address: " + IPAddressString + " Serialnumber: " + chassisSerialNumber);
+                    Logging.Logs.writeToLogfile(log);
                 }
                 catch (Exception ex)
                 {
@@ -111,17 +116,20 @@ namespace CiscoDatabaseProgram.Functions.SerialNumbers
         }
         public static string findCereal(string originalstring) // Find chassis serial number ** command: show diag **
         {
-            string searchPattern = "\r\n\tHardware Revision"; // this string is infront of the CHASSIS serial number
-            if (originalstring.Contains(searchPattern))
+            List<string> log = new List<string>();
+            string searchPattern = "Chassis Serial Number    :"; // this string is infront of the CHASSIS serial number
+            if (originalstring.Contains(searchPattern))// checks 
             {
                 int indexPattern = originalstring.IndexOf(searchPattern); // index of the pattern
-                int startIndex = indexPattern - 11; // 11 is the default length of a serialnumber
-                string chassisSerialNumber = originalstring.Substring(startIndex, 11);
+                int startIndex = indexPattern + searchPattern.Length + 1; // adding 1 for space after :
+                string chassisSerialNumber = originalstring.Substring(startIndex, 11); //serialnumber is received by substring orignalstring
                 return chassisSerialNumber;
             }
             else
             {
                 Console.WriteLine("Serienummber kon niet worden achterhaalt");
+                log.Add("ERROR - could not find serialnumber using substring method");
+                Logging.Logs.writeToLogfile(log);                
             }
             return null;
         }
