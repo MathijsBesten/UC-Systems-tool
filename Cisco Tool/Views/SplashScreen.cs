c5593faf0e4e7c2d33dacce1fefba797e88f3ce1
Mathijs <m88_db@hotmail.com>
2016-10-27 12:20:52 +0200

    upped the time of spashscreen to 250 ms

--- Cisco Tool/Views/SplashScreen.cs
+++ Cisco Tool/Views/SplashScreen.cs
@@ -24,7 +24,7 @@ namespace Cisco_Tool.Views
         private void SplashScreen_Shown(object sender, EventArgs e)
         {
             mainTimer = new Timer();
-            mainTimer.Interval =100;
+            mainTimer.Interval =250;
             mainTimer.Start();
             mainTimer.Tick += mianTimer_Tick;
             mainform = new MainForm();
