f873539ec3410a2e3e399ee4cdd77bef51163564
Mathijs <m88_db@hotmail.com>
2016-11-21 15:29:03 +0100

    added private class to github removed all the unused references

---
+++ Cisco Tool/Values/PrivateValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cisco_Tool.Values
+{
+    class PrivateValues
+    {
+        public static string OwnServerServerQuery = "select * from dbo.router ORDER BY router_friendlyname";
+        public class router
+        {
+            public int routerId { get; set; }
+            public string routerName { get; set; }
+            public string routerAlias { get; set; }
+            public string routerAddress { get; set; }
+            public string routerActivate { get; set; }
+            public string routerSerialnumber { get; set; }
+            public int routerMainDB { get; set; }
+
+        }
+    }
+}
